2de55d49f0f4c30353abec3f0a4d7ab3479221b8
joshuamenezes <joshua.menezes@outlook.com>
2022-02-28 22:27:24 -0500

    [UT-24] Created placeholder views for basic CRUD operations on listings

--- src/listings/views.py
+++ src/listings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from listings.models import Listing
+from django.views.generic import FormView, ListView, DetailView, UpdateView
 # Create your views here.
+
+class AddListing(FormView):
+    pass
+
+class DeleteListing():
+    pass
+
+class UpdateListing(FormView):
+    pass
+
+class DisplayListings(ListView):
+    pass

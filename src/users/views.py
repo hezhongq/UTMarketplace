08169469280efd484f5bc5324bffa1bcf3fc44e9
baicaitomato <58008976+baicaitomato@users.noreply.github.com>
2022-03-02 13:09:40 -0500

    [UT-42]complete views

--- src/users/views.py
+++ src/users/views.py
@@ -129,6 +129,20 @@ def reset_password(response):
     return render(response, "users/pwd_retrieval.html", {'form': form, 'error': error})
 
 
+def change_password(response):
+    if not response.user.is_authenticated:
+        return render(response, "users/result.html", {'error': "please login"})
+    if response.method == "POST":
+        form = PasswordChangeForm(user=response.user, data=response.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.update_session_auth_hash(response, user)
+            return redirect('/users/profile/{0}'.format(user.id))
+    else:
+        form = PasswordChangeForm(user=response.user)
+    return render(response, "users/change_profile_password.html", {'form': form})
+
+
 '''===helpers==='''
 
 

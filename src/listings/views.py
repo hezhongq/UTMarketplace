4bad7a4fb299b57800a3ba536bc6192ae4488746
Arush Awasthi <arush@Arushs-MacBook-Pro.local>
2022-03-03 21:53:00 -0500

    [UT-3]: Overrode get_queryset in listings/view.py to support filtering by cost range and category.

--- src/listings/views.py
+++ src/listings/views.py
@@ -18,3 +18,15 @@ class DisplayListings(ListView):
     model = Listing
     context_object_name = "listing"
     template_name = "listings/display_listings.html"
+    
+    def get_queryset(self):
+        cost_from = self.request.GET.get('cost_from', 'give-default-value')
+        cost_to = self.request.GET.get('cost_to', 'give-default-value')
+        ctgry = self.request.GET.get('category', 'give-default-value')
+        
+        new_context = Listing.objects.filter(
+            price__range=(cost_from, cost_to)
+        ).filter(
+            category=ctgry
+        )
+        return new_context

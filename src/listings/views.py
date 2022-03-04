6a98403668e0107fce6ac948ebcc7a48099751e7
joshuamenezes <joshua.menezes@outlook.com>
2022-03-03 19:03:03 -0500

    [UT-24] [UT-26] Created placeholders for basic CRUD related operations concerning listings. URLs must be defined first

--- src/listings/views.py
+++ src/listings/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpRequest, HttpResponse, JsonResponse
+
+from listings.forms.add_listing import AddListingForm
+
 from .models import Listing
 from django.views.generic import FormView, ListView, DetailView, UpdateView, CreateView, DeleteView
 # Create your views here.
@@ -12,15 +15,31 @@ def home(response):
 
 class AddListing(FormView):
     template_name = 'listings/add_listing.html'
+    form_class = AddListingForm
 
+    def form_valid(self, form):
+        # Create the new listing here after validating data. Redirect to success URL if listing was successfully created
+        pass
+
+# Ensure that only the user who created this post can delete it
 class DeleteListing(DeleteView):
     model = Listing
+
+    def get_object(self):
+        # Look for the object that belongs to the user here
+        pass
 
     def get_success_url(self):
         pass
 
-class UpdateListing(FormView):
-    pass
+class UpdateListing(UpdateView):
+    model = Listing
+    context_object_name = 'listing'
+    template_name = 'listings/edit_listing.html'
+
+    # Specify the success url here
+    def get_success_url(self):
+       pass 
 
 class DisplayListings(ListView):
     model = Listing

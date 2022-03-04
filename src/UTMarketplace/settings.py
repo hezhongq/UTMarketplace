2b0f9a23cb29b5f9cbfa1d213e55b5f5b5d4ba3f
shearsmurf <chriss.fds@gmail.com>
2022-03-04 08:43:45 -0500

    [UT-55] Fixed a broken link

--- src/UTMarketplace/settings.py
+++ src/UTMarketplace/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
-MEDIA_URL = '/media/' # Public URL at the browser
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') # Directory where uploaded media is saved.
+MEDIA_URL = '/images/' # Public URL at the browser
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]

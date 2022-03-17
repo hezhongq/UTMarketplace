6648ef9fd3dbf6662111b60b6076923a275390ed
arushawasthi-uoft <arush.awasthi@mail.utoronto.ca>
2022-03-16 22:16:59 -0400

    Commit for rebase

--- src/listings/views.py
+++ src/listings/views.py
@@ -73,4 +73,15 @@ class SingleListing(DetailView):
     model = Listing
     context_object_name = "listing"
     template_name = "listings/single_listing.html"
-
+    
+    def get_queryset(self):
+        cost_from = self.request.GET.get('cost_from', 'give-default-value')
+        cost_to = self.request.GET.get('cost_to', 'give-default-value')
+        ctgry = self.request.GET.get('category', 'give-default-value')
+        
+        new_context = Listing.objects.filter(
+            price__range=(cost_from, cost_to)
+        ).filter(
+            category=ctgry
+        )
+        return new_context

712038939086198fe203b0c9e44c85ae2dcc45bf
joshuamenezes <joshua.menezes@outlook.com>
2022-03-17 20:30:49 -0400

    [UT-15] Modified redirects to stay on same page

--- src/listings/views.py
+++ src/listings/views.py
@@ -78,8 +78,13 @@ def bookmark_listing(request, pk):
         # The user has already bookmarked this listing
         if bookmark.listing == given_listing and bookmark.owner == request.user:
             Bookmark.objects.get(id=bookmark.id).delete()
+
+            if request.POST['url_type'] == 'all_listings':
+                return redirect('/listings/')
             return redirect(f'/listings/{pk}/details/')
 
     new_bookmark = Bookmark(owner=request.user, listing=given_listing)
     new_bookmark.save()
+    if request.POST['url_type'] == 'all_listings':
+        return redirect('/listings/')
     return redirect(f'/listings/{pk}/details/')
